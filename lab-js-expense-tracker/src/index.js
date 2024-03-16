dae87e1689b2e71a7ed4442e9a450d7ff5874de2
Joshua George <horheyinc8@gmail.com>
2024-03-16 10:41:39 +0100

    lab solution

---
+++ lab-js-expense-tracker/src/index.js
@@ -0,0 +1,53 @@
+// Entry
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
+
+// Income
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
+// Expense
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
+
+// Budget
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(aNewEntry) {
+    this.entries.push(aNewEntry);
+  }
+  getCurrentBalance() {
+    console.log(this.entries);
+    let total = 0;
+    this.entries.forEach((oneEntry) => {
+      if (oneEntry.type === "income") {
+        total += oneEntry.amount;
+      } else {
+        total -= oneEntry.amount;
+      }
+    });
+    console.log(total);
+    return total;
+  }
+}
